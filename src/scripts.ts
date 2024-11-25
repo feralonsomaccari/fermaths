99fc70d4b43e82ea434d4c29ae11dd711f8110ab
maccarif <fernando.maccari@barrons.com>
2024-11-25 11:34:53 +0200

    fix: wrong key listeners handling

--- src/scripts.ts
+++ src/scripts.ts
@@ -31,6 +31,82 @@ document.addEventListener("DOMContentLoaded", () => {
     return;
   }
 
+  const currentOperands = {
+    firstOperandValue: 0,
+    secondOperandValue: 0,
+    operator: "",
+  };
+
+  const handleOperation = (event: KeyboardEvent) => {
+    if (
+      event.key === "Enter" &&
+      document.activeElement === userResult &&
+      userResult.value
+    ) {
+      let result: number;
+
+      const { firstOperandValue, secondOperandValue, operator } =
+        currentOperands;
+      switch (operator) {
+        case "+":
+          result = firstOperandValue + secondOperandValue;
+          break;
+        case "-":
+          result = firstOperandValue - secondOperandValue;
+          break;
+        case "x":
+          result = firstOperandValue * secondOperandValue;
+          break;
+        case "/":
+          result = firstOperandValue / secondOperandValue;
+          break;
+        default:
+          return;
+      }
+
+      if (result === parseFloat(userResult.value)) {
+        animationResult.classList.add("animation-result");
+        animationResult.textContent = "CORRECT!";
+        score.correct = score.correct + 1;
+        scoreCorrectEl.textContent = score.correct.toString();
+        animationResult.addEventListener("animationend", () => {
+          animationResult.style.opacity = "0";
+          animationResult.style.transform = "scale(0)";
+          generateRandomOperation();
+        });
+      } else {
+        score.incorrect = score.incorrect + 1;
+        scoreIncorrectEl.textContent = score.incorrect.toString();
+        userResult.classList.add("animation-text-error");
+        userResult.addEventListener("animationend", () => {
+          userResult.classList.remove("animation-text-error");
+        });
+      }
+    }
+  };
+
+  const generateRandomOperation = () => {
+    const operators = ["+", "-", "x"];
+
+    const randomOperator =
+      operators[Math.floor(Math.random() * operators.length)];
+
+    operator.textContent = randomOperator;
+
+    const largestNumberLimit = 100;
+    const firstOperandValue = Math.floor(Math.random() * largestNumberLimit);
+    const secondOperandValue = Math.floor(Math.random() * largestNumberLimit);
+
+    firstOperand.textContent = firstOperandValue.toString();
+    secondOperand.textContent = secondOperandValue.toString();
+
+    currentOperands.firstOperandValue = firstOperandValue;
+    currentOperands.secondOperandValue = secondOperandValue;
+    currentOperands.operator = randomOperator;
+
+    userResult.value = "";
+  };
+
   document.addEventListener("keydown", (event) => {
     if (event.code === "KeyR") {
       generateRandomOperation();
@@ -48,67 +124,7 @@ document.addEventListener("DOMContentLoaded", () => {
     userResult.value = userResult.value.replace(/[^0-9-]/g, "");
   });
 
-  const generateRandomOperation = () => {
-    const operators = ["+", "-", "x"];
-
-    const randomOperator =
-      operators[Math.floor(Math.random() * operators.length)];
-
-    operator.textContent = randomOperator;
-
-    const largestNumberLimit = 100;
-    const firstOperandValue = Math.floor(Math.random() * largestNumberLimit);
-    const secondOperandValue = Math.floor(Math.random() * largestNumberLimit);
-
-    firstOperand.textContent = firstOperandValue.toString();
-    secondOperand.textContent = secondOperandValue.toString();
-    const handleOperation = (event: KeyboardEvent) => {
-      if (
-        event.key === "Enter" &&
-        document.activeElement === userResult &&
-        userResult.value
-      ) {
-        let result: number;
-        switch (randomOperator) {
-          case "+":
-            result = firstOperandValue + secondOperandValue;
-            break;
-          case "-":
-            result = firstOperandValue - secondOperandValue;
-            break;
-          case "x":
-            result = firstOperandValue * secondOperandValue;
-            break;
-          case "/":
-            result = firstOperandValue / secondOperandValue;
-            break;
-          default:
-            return;
-        }
-
-        if (result === parseFloat(userResult.value)) {
-          animationResult.classList.add("animation-result");
-          animationResult.textContent = "CORRECT!";
-          score.correct = score.correct + 1;
-          scoreCorrectEl.textContent = score.correct.toString();
-          animationResult.addEventListener("animationend", () => {
-            animationResult.style.opacity = "0";
-            animationResult.style.transform = "scale(0)";
-            generateRandomOperation();
-          });
-        } else {
-          score.incorrect = score.incorrect + 1;
-          scoreIncorrectEl.textContent = score.incorrect.toString();
-          userResult.classList.add("animation-text-error");
-          userResult.addEventListener("animationend", () => {
-            userResult.classList.remove("animation-text-error");
-          });
-        }
-      }
-    };
-    document.addEventListener("keydown", handleOperation);
-    userResult.value = "";
-  };
+  document.addEventListener("keydown", handleOperation);
 
   generateRandomOperation();
 });
